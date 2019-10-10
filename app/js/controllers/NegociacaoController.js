7d3d395ca9376a60c5e9abe0ff4a370106c691a5
1744952 <andersonsutil.off@gmail.com>
2019-10-10 11:44:31 -0300

    Substituição dos Types

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -3,20 +3,20 @@ class NegociacaoController {
         this._negociacoes = new Negociacoes();
         this._negociacoesView = new NegociacoesView('#negociacoesView');
         this._mensagemView = new MensagemView('#mensagemView');
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
         this._negociacoes.adiciona(negociacao);
         this._negociacoesView.update(this._negociacoes);
-        console.log(negociacao.data);
+        console.log(negociacao.data); //<--- testando as saida no console do Browser
         this._mensagemView.update('Deu certo carai');
         //this._negociacoes.paraArray().forEach(negociacao =>{
-        //  console.log(negociacao.data);
+        //  console.log(negociacao.data);                     //< ----  array com objetos para teste
         //   console.log(negociacao.quantidade);
         //  console.log(negociacao.valor);
         //    console.log(negociacao.volume);
--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,16 +1,16 @@
 class NegociacaoController {
 
-    private _inputData: HTMLInputElement;
-    private _inputQuantidade: HTMLInputElement;
-    private _inputValor: HTMLInputElement;
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
-        this._inputData = <HTMLInputElement>document.querySelector('#data');
-        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
-        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
      
     }
@@ -20,17 +20,19 @@ class NegociacaoController {
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            new Date(this._inputData.value.replace(/-/g, ',')), 
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
+            new Date(this._inputData.val().replace(/-/g, ',')), 
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
         );
 
     this._negociacoes.adiciona(negociacao);
     this._negociacoesView.update(this._negociacoes)
-    console.log(negociacao.data);
+    
+    console.log(negociacao.data);  //<--- testando as saida no console do Browser
+    
     this._mensagemView.update('Deu certo carai');
     //this._negociacoes.paraArray().forEach(negociacao =>{
-                  //  console.log(negociacao.data);
+                  //  console.log(negociacao.data);                     //< ----  array com objetos para teste
                   //   console.log(negociacao.quantidade);
                   //  console.log(negociacao.valor);
                   //    console.log(negociacao.volume);
